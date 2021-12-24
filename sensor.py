ee4ed68d2903bc723574a811c2a4dfcdc4388e12
Imanuel Bertrand <imanuel@bertrand.one>
2021-12-24 11:49:08 +0100

    Some logic

--- sensor.py
+++ sensor.py
@@ -1,11 +1,19 @@
+import random
+
 from config import Config
 
 
 class Sensor:
     name: str
     group: str
-    Type: str
+    backend: str
     config: dict
+
+    def __init__(self, name: str, group: str, backend: str, config: dict):
+        self.name = name
+        self.group = group
+        self.backend = backend
+        self.config = config
 
     def read_i2c(self):
         address = self.get_config("i2c.address")
@@ -15,7 +23,9 @@ class Sensor:
     def get_config(self, key, default=None):
         return Config.get(key=key, default=default, data=self.config)
 
-    def get_value(self):
-        if self.Type == 'i2c':
+    def read(self):
+        if self.backend == 'i2c':
             return self.read_i2c()
+        if self.backend == 'random':
+            return random.randint(0, 100)
         raise Exception("Sensor type undefined")
--- sensormonitor.py
+++ sensormonitor.py
@@ -1,14 +1,59 @@
+import json
+import logging
+
+import requests
+
 from config import Config
+from sensor import Sensor
 
 
 class SensorMonitor:
     sensors = None
 
     @staticmethod
+    def send_data(data) -> bool:
+        response = requests.post(
+            url=Config.get("Server.Address"),
+            json=data,
+            auth=(
+                Config.get("Server.Username"),
+                Config.get("Server.Password")
+            )
+        )
+        if response.status_code != 200:
+            logging.error("Received status code " + str(response.status_code) + " with body: " + response.text)
+            return False
+        try:
+            result = json.loads(response.text)
+        except ValueError as err:
+            logging.error("Response could not be decoded: " + response.text)
+            return False
+
+        if "result" in result and result["result"] == "good":
+            return True
+
+        logging.error("Unsuccessful call: " + str(result))
+
+    @staticmethod
+    def read_sensors():
+        for sensor in SensorMonitor.sensors:
+            print(sensor.name + " in " + sensor.group + ": " + str(sensor.read()))
+            pass
+
+    @staticmethod
+    def setup_sensors():
+        SensorMonitor.sensors = []
+        idents = {}
+        for data in Config.get("Sensors"):
+            ident = data["Name"] + "///" + data["Group"]
+            if ident in idents:
+                raise Exception("There can not be multiple sensors with the same group and name!")
+            idents[ident] = ident
+            SensorMonitor.sensors.append(Sensor(data["Name"], data["Group"], data["Backend"], data))
+
+    @staticmethod
     def run():
-        Config.dump()
-        sensors = Config.get("Sensors")
-        for name in sensors:
-            print(name)
+        SensorMonitor.setup_sensors()
+        SensorMonitor.read_sensors()
 
         pass
